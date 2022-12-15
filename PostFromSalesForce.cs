bbd6a8e4bfb38310b664ca5d1ba6b6daf62670f7
Jens Tinglev <jens.tinglev@magello.se>
2022-12-15 10:18:08 +0100

    Initial commit

---
+++ PostFromSalesForce.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Magello.SalesForceToTeamTailor
+{
+    public class PostFromSalesForce
+    {
+        private readonly ILogger _logger;
+
+        public PostFromSalesForce(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<PostFromSalesForce>();
+        }
+
+        [Function("PostFromSalesForce")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("PostFromSalesForce function processing a request..");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
