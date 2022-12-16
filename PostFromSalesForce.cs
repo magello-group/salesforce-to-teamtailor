5333ccf589af0e80b88c8cd90ae3ce817866f698
Jens Tinglev <jens.tinglev@magello.se>
2022-12-16 12:42:44 +0100

    SalesForce function update

--- PostFromSalesForce.cs
+++ PostFromSalesForce.cs
@@ -15,16 +15,45 @@ namespace Magello.SalesForceToTeamTailor
         }
 
         [Function("PostFromSalesForce")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "PUT", "POST", "DELETE")] HttpRequestData req)
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "PUT", "POST", "DELETE")] HttpRequestData req)
         {
             _logger.LogInformation("PostFromSalesForce function processing a request..");
 
+            SalesForceData jsonData;
+            try {
+                jsonData = await System.Text.Json.JsonSerializer.DeserializeAsync<SalesForceData>(req.Body);
+            }
+            catch (Exception e) {
+                _logger.LogError(e, "Error when deserializing json body");
+                var errorResponse = req.CreateResponse(HttpStatusCode.BadRequest);
+                await errorResponse.WriteAsJsonAsync(new ErrorData() { error = "bad json data provided "});
+                return errorResponse;
+            }
+
+            _logger.LogInformation($"Recieved data: {jsonData}");
+
             var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-            response.WriteString("Welcome to Azure Functions!");
-
+            await response.WriteAsJsonAsync(jsonData);
             return response;
+        }
+
+        private class SalesForceData {
+            public string? id { get; set; }
+            public string? name { get; set; }
+            public string? accountName { get; set; }
+            public string? lastAnswerDatePart { get; set; }
+            public string? agreementPeriod { get; set; }
+            public string? workPlace { get; set; }
+            public string? description { get; set; }
+
+            public override string ToString()
+            {
+                return $"id: {id}, name: {name}";
+            }
+        }
+
+        private class ErrorData {
+            public string? error { get; set; }
         }
     }
 }
