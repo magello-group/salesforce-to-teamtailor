7065dc20a6113d369046ac827dfdbc0484404ec9
Jens Tinglev <jens.tinglev@magello.se>
2023-01-09 12:42:42 +0100

    Further work on envs

--- src/Utils/Envs.cs
+++ src/Utils/Envs.cs
@@ -14,32 +14,32 @@ namespace Magello {
         public static readonly string E_AzStorageAccountKey = "TABLE_STORAGE_KEY";
         public static readonly string E_SalesForceCustomFieldId = "SFID_CUSTOM_FIELD_ID";
         public static readonly string E_TeamTailorBaseUrl = "TEAMTAILOR_BASE_URL";
+        
         public static string? GetEnvVar(string envVarName) {
             return Environment.GetEnvironmentVariable(envVarName) ?? null;
         }
 
-                private static bool EnvVarIsSet(string envVarName) {
-            return !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable(envVarName) ?? "");
+        private static bool EnvVarIsSet(string envVarName) {
+            return !string.IsNullOrWhiteSpace(GetEnvVar(envVarName));
         }
         
         // Pre-flight env checks
         public static void PreFlightEnvChecks() {
             var envs = new List<string> {
-                "TABLE_STORAGE_URI", 
-                "TABLE_STORAGE_NAME", 
-                "TABLE_STORAGE_KEY", 
-                "SFID_CUSTOM_FIELD_ID", 
-                "TEAMTAILOR_BASE_URL", 
-                "TEAMTAILOR_API_TOKEN", 
-                "SALESFORCE_API_HOST", 
-                "SALESFORCE_TOKEN_ENDPOINT", 
-                "SALESFORCE_CLIENT_KEY",
-                "SALESFORCE_CLIENT_SECRET",
+                E_TeamTailorApiToken, 
+                E_SalesForceApiHost, 
+                E_SalesForceApiTokenEndpoint, 
+                E_SalesForceApiClientKey, 
+                E_SalesForceApiClientSecret, 
+                E_AzStorageAccountUri, 
+                E_AzStorageAccountName, 
+                E_AzStorageAccountKey, 
+                E_SalesForceCustomFieldId,
+                E_TeamTailorBaseUrl,
             };
-            foreach (var e in envs)
-            {
-                Debug.Assert(EnvVarIsSet(e), $"Environment variable {e} should not be empty");
-            }
+            envs.ForEach(e => 
+                Debug.Assert(EnvVarIsSet(e), $"Environment variable {e} should not be empty")
+            );
         }
 
     }
